538c379b52e49fb186538099a20943727d502778
gaga.gournay@gmail.com <gaga.gournay@gmail.com>
2025-05-31 12:01:03 +0200

    shop skill preview

--- ppjjgg4/Assets/Scripts/Plant.cs
+++ ppjjgg4/Assets/Scripts/Plant.cs
@@ -14,7 +14,7 @@ public class Plant : ScriptableObject {
     [SerializeField] private int score = 1;
     [SerializeField] private int growthTime = 3;
     [field: SerializeField] public Plot plot { get; private set; }
-    [SerializeReference, SubclassPicker] ISkill skill;
+    [SerializeReference, SubclassPicker] public ISkill skill;
     public int day;
     public bool hasMatured;
 
